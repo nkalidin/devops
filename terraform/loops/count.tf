ad1a26e93bb3fd1d80410b9c219a93d33a6795a8
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2022-11-16 19:21:56 +0530

    Create count.tf

---
+++ terraform/loops/count.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "hexa-rg" {
+  count    = length(var.rg)
+  location = "eastus"
+  name     = var.rg[count.index]
+}
+
+variable "rg" {
+  type    = list(string)
+  default = ["hexa1", "hexa2", "hexa3"]
+}
+
+
+resource "azurerm_resource_group" "hexagon-rg" {
+  count    = length(var.hexa1)
+  location = "southindia"
+  name     = var.hexa1[count.index]
+
+}
+
+variable "hexa1" {
+  type    = list(string)
+  default = ["h1", "h2", "h3", "h4"]
+
+}
