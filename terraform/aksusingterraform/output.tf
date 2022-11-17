19a77065339aab0fe8eea1a2312e06a347eace4a
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2022-11-17 17:36:04 +0530

    Create output.tf

---
+++ terraform/aksusingterraform/output.tf
@@ -0,0 +1,6 @@
+resource "local_file" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.k8s]
+  filename   = "kubeconfig"
+  content    = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
+
