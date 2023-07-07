157484769875cf3ecac24438111ee294440f0e3e
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2023-07-07 13:09:20 +0530

    Update install.sh

--- istio/install.sh
+++ istio/install.sh
@@ -11,3 +11,18 @@ kubectl exec "$(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.
 vi samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl create -f samples/bookinfo/networking/bookinfo-gateway.yaml
 istioctl analyze
+ kubectl expose deployment/productpage-v1 --name=plb1 --type=LoadBalancer --port=9080
+http://20.237.77.74:9080/productpage
+20.237.74.7
+
+   44  kubectl apply -f samples/addons
+   45  kubectl rollout status deployment/kiali -n istio-system
+   46  kubectl get ns
+   48  kubectl get deployments -n istio-system
+   49  kubectl get pods -n istio-system
+   50  kubectl get service -n istio-system
+   53  kubectl expose deployment/kiali --name=kiali1 --type=LoadBalancer --port=20001 -n istio-system
+   54  kubectl get service -n istio-system
+
+
+http://20.81.122.212:20001/kiali
